0969aa25680a6fe77eae1baf3c71a773d096a78f
csm <csm009177@gmail.com>
2023-12-25 14:42:43 +0900

    install express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('sofia hellow')
+// import express
+import express from 'express';
+import rootRoute from './routes/rootRoute.js';
+
+const app = express();
+const port = 3217;
+
+app.use(express.static('public'));
+app.use(rootRoute); // 라우터 추가
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
