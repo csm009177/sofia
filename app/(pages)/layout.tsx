4db7fe089ba3c0aa78573b0b7f956f316d1ebf9b
csm <csm009177@gmail.com>
2024-02-08 14:57:00 +0900

    서치 파람을 이용한 페이지 이동

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,20 +1,34 @@
-'use client'
+"use client";
 
 import "../globals.css";
 import { ChildrenProps } from "./ChildrenProps";
-import { useState } from "react";
-import Header from './ui/Header';
+import { useEffect, useState } from "react";
+import Header from "./ui/Header";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 
-
-export default function RootLayout({ children }:ChildrenProps) {
+export default function RootLayout({ children }: ChildrenProps) {
+  useParams;
   const [url, setUrl] = useState(window.location.pathname); // 현재 URL 경로를 가져옴
+  const [showHeader, setShowHeader] = useState(true);
+  const href = window.location.href;
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    if (url === "/") {
+      setShowHeader(!showHeader);
+    }
+  }, [pathname, searchParams]);
+
   return (
-    <div  className='flex flex-col justify-between h-screen w-screen overflow-hidden'>
-      {url=== '/' && <Header/>}     
-      {/* {url === "/" && <Header/>}
-      {url === "/" && <Header/>}
-      {url === "/" && <Header/>} */}
+    <div className="flex flex-col justify-between h-screen w-screen overflow-hidden">
+      {showHeader ? "" : <Header />}
       {children}
     </div>
-  )
+  );
 }
