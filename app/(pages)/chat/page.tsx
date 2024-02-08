ff0353decfc4b1427c27a793cdba569e5871e450
csm <csm009177@gmail.com>
2024-02-08 16:49:46 +0900

    chat page에서 data가 전송되도록 시도

--- app/(pages)/chat/page.tsx
+++ app/(pages)/chat/page.tsx
@@ -1,38 +1,36 @@
 "use client";
 
-import { useCallback, useState } from "react";
-
-
 // Chat 컴포넌트
+import { useState } from "react";
+
 export default function Chat() {
-  const [message, setMessage] = useState("");
+  const [chatMessage, setChatMessage] = useState("");
 
-  const handleMessageSubmit = async (e) => {
-    e.preventDefault();
-
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
     try {
       await fetch('/chatlogForm', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ message }),
+        body: JSON.stringify({ chatContents: chatMessage }),
       });
-
-      // 입력 후 입력란 초기화
-      setMessage("");
+      // 채팅 메시지를 서버로 전송한 후 입력 필드 초기화
+      setChatMessage("");
     } catch (error) {
-      console.error("Error sending message:", error);
+      console.error("Error sending chat message:", error);
     }
   };
 
   return (
     <div className="flex flex-col justify-between h-full w-screen overflow-hidden">
-      <form onSubmit={handleMessageSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          value={chatMessage}
+          onChange={(e) => setChatMessage(e.target.value)}
         />
         <button type="submit">Send</button>
       </form>
