3a7c77b932458ea471bd62cf838d68e1601b122b
csm <csm009177@gmail.com>
2024-02-08 16:40:51 +0900

    chating page 만들기 시작

--- app/(pages)/chat/page.tsx
+++ app/(pages)/chat/page.tsx
@@ -1,9 +1,41 @@
-import Image from "next/image";
+"use client";
 
+import { useCallback, useState } from "react";
+
+
+// Chat 컴포넌트
 export default function Chat() {
+  const [message, setMessage] = useState("");
+
+  const handleMessageSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await fetch('/chatlogForm', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      // 입력 후 입력란 초기화
+      setMessage("");
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-between h-full w-screen overflow-hidden">
-      Chating Page
+      <form onSubmit={handleMessageSubmit}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
