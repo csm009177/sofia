12eccfeaebfebe930e6b46eec7e782821941429d
csm <csm009177@gmail.com>
2024-02-08 13:06:29 +0900

    헤더 만들기

--- app/(pages)/ui/Header.tsx
+++ app/(pages)/ui/Header.tsx
@@ -1,17 +1,16 @@
-import UrlButton from './UrlButton';
-
+import UrlButton from "./UrlButton";
 
 export default function Header() {
   return (
-    <div>
-      <div className='flex flex-row h-32 justify-between p-7'>
-      <UrlButton url="chatlog" title="chat"/>
-      <UrlButton url="planer" title="planer"/>
-      <UrlButton url="album" title="album"/>
+    <div className="flex flex-row w-screen justify-between">
+      <div className="flex flex-row h-24 p-5">
+        <UrlButton url="chatlog" title="chat" />
+        <UrlButton url="planer" title="planer" />
+        <UrlButton url="album" title="album" />
       </div>
-      <div>
-        <UrlButton url="login" title="login"/> 
-        <UrlButton url="signin" title="signin"/> 
+      <div className="flex flex-row justify-between p-5">
+        <UrlButton url="login" title="login" />
+        <UrlButton url="signin" title="signin" />
       </div>
     </div>
   );
