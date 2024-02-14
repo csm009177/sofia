22b54b91255b81929102dba5a3a5eb646e161991
csm <csm009177@gmail.com>
2024-02-14 14:48:22 +0900

    header 컴포넌트 트  토큰 유무에 따라 로그인 버튼 나타나게 하기

--- app/(pages)/ui/Header.tsx
+++ app/(pages)/ui/Header.tsx
@@ -1,6 +1,16 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import UrlButton from "./UrlButton";
 
 export default function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    setIsLoggedIn(!!token); // 토큰이 존재하면 true, 아니면 false로 설정
+  }, []);
+
   return (
     <div className="flex flex-row w-screen justify-between">
       <div className="flex flex-row h-24 w-1/4 p-5 justify-between">
@@ -8,10 +18,10 @@ export default function Header() {
         <UrlButton url="planer" title="planer" />
         <UrlButton url="album" title="album" />
       </div>
-      <div className="flex flex-row h-24 w-1/6 p-5 justify-between">
-        <UrlButton url="login" title="login" />
-        <UrlButton url="signup" title="signup" />
-      </div>
+        <div className="flex flex-row h-24 w-1/6 p-5 justify-between">
+          <UrlButton url="login" title="login" />
+          <UrlButton url="signup" title="signup" />
+        </div>
     </div>
   );
 }
