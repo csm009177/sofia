f57e6c3e5f6db829ec79c190ee39acfcba5847b4
csm <141980543+csm009177@users.noreply.github.com>
2025-10-05 01:30:19 +0900

    전체 컴포넌트 es6방식 적용 - 추후 더 빠른방법으로 전환필요

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import { 요소 } from './render.js';
 
 export function 푸터생성(){
-new 요소('root', '푸터', 'div', 'rgba(50, 50, 50, 255)', '100vw', '5vh', '', { style: 'display:flex; justify-content:center; align-items:center;' });
+    new 요소('root', '푸터', 'div', 'rgba(50, 50, 50, 255)', '100vw', '5vh', '', { style: 'display:flex; justify-content:center; align-items:center;' });
 }
